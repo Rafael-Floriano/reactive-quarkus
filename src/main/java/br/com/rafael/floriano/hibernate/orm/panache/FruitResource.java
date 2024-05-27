001d40cfe1e09397780203e7bbfe6b555a09e52d
rafael <rafael.florianoc9@gmail.com>
2024-05-26 23:01:56 -0300

    FEAT: created new method to persist new Fruit

--- src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
+++ src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
@@ -1,10 +1,13 @@
 package br.com.rafael.floriano.hibernate.orm.panache;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -23,4 +26,9 @@ public class FruitResource {
         return Fruit.findById(id);
     }
 
+    @POST
+    public Uni<Response> create(Fruit fruit) {
+        return Panache.withTransaction(fruit::persist)
+                .replaceWith(Response.ok(fruit).status(Response.Status.CREATED)::build);
+    }
 }
