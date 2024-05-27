0defb55a04c6c4d9a168f0363577dc0f4d95587e
rafael <rafael.florianoc9@gmail.com>
2024-05-26 22:35:57 -0300

    FEAT: added FruitResource with @GET mapping to view all fruits in database

---
+++ src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
@@ -0,0 +1,20 @@
+package br.com.rafael.floriano.hibernate.orm.panache;
+
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.util.List;
+
+@Path("/fruits")
+@ApplicationScoped
+public class FruitResource {
+
+    @GET
+    public Uni<List<Fruit>> get() {
+        return Fruit.listAll(Sort.by("name"));
+    }
+
+}
