44e8c5b0816cbc87b44d9906a767cb3cea0322ba
rafael <rafael.florianoc9@gmail.com>
2024-05-26 23:15:00 -0300

    FEAT: adding method to update name of fruits

--- src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
+++ src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
@@ -6,6 +6,7 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
@@ -30,5 +31,16 @@ public class FruitResource {
     public Uni<Response> create(Fruit fruit) {
         return Panache.withTransaction(fruit::persist)
                 .replaceWith(Response.ok(fruit).status(Response.Status.CREATED)::build);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Uni<Response> update(Long id, Fruit fruit) {
+        return Panache
+                .withTransaction(
+                        () -> Fruit.<Fruit> findById(id)
+                                .onItem().ifNotNull().invoke(entity -> entity.name = fruit.name)
+                )
+                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build());
     }
 }
