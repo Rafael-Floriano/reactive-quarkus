c4929be80da286d3e81efcd876db4c36ca4ba90d
rafael <rafael.florianoc9@gmail.com>
2024-05-26 23:28:55 -0300

    FEAT: implemented Delete route

--- src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
+++ src/main/java/br/com/rafael/floriano/hibernate/orm/panache/FruitResource.java
@@ -4,13 +4,12 @@ import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
+
+import static jakarta.ws.rs.core.Response.Status.*;
 
 @Path("/fruits")
 @ApplicationScoped
@@ -42,5 +41,14 @@ public class FruitResource {
                                 .onItem().ifNotNull().invoke(entity -> entity.name = fruit.name)
                 )
                 .onItem().ifNotNull().transform(entity -> Response.ok(entity).build());
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Uni<Response> delete(Long id) {
+        return Panache.withTransaction(() -> Fruit.deleteById(id))
+                .map(deleted -> deleted
+                        ? Response.ok().status(NO_CONTENT).build()
+                        : Response.ok().status(NOT_FOUND).build());
     }
 }
